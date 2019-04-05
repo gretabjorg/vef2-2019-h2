b4a73f16d9237b524670452e90a8793a04621fec
Gunnar Ingi Stefansson <gis20@hi.is>
2019-04-05 23:41:09 +0000

    Gróf uppsetning á forsíðu route

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -3,12 +3,17 @@ import Helmet from 'react-helmet';
 
 import './Home.scss';
 
+import Products from '../../components/products/Products';
+import Button from '../../components/button/Button';
+
 export default function Home() {
 
   return (
     <Fragment>
       <Helmet title="Forsíða" />
-      <p>Home</p>
+      <Products limit={6}/>
+      <Button children={'Skoða alla flokka'} />
+      {/* <Categories /> */}
     </Fragment>
   );
 }
