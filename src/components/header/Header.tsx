d0f71914d281ef74dd18150269e4f678202523ef
Gunnar Ingi Stefansson <gis20@hi.is>
2019-04-05 16:03:05 +0000

    css á header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,7 +10,10 @@ export default function Home() {
         <h1 className="header__title">
           <Link className="header__titleLink" to="/">Vefforritunarbúðin</Link>
         </h1>
-        <NavLink activeClassName="header__link--selected" exact to="/cart">Karfa</NavLink>
+        <div className="header__nav">
+          <NavLink className="header__link" activeClassName="header__link header__link--selected" exact to="/products">Nýjar vörur</NavLink>
+          <NavLink className="header__link" activeClassName="header__link header__link--selected" exact to="/cart">Karfa</NavLink>
+        </div>
       </div>
     </header>
   );
