b997225f8e55d8f38bdd066829d37a94355307fd
Gunnar Ingi Stefansson <gis20@hi.is>
2019-04-08 16:05:15 +0000

    Data fetcherar fyrir products og categories

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,18 @@ import { IProduct } from './types';
 // Sækja slóð á API úr env
 const baseurl:string | undefined = process.env.REACT_APP_API_URL;
 
+function getPage(limit: Number, offset: Number) {
+  return `${
+    limit
+    ? offset 
+      ? `?limit=${limit}&offset=${offset}`
+      : `?limit=${limit}`
+    : offset
+      ? `?offset=${offset}`
+      : ''
+  }`;
+}
+
 async function getProduct(id: number | string) : Promise<IProduct> {
   // todo sækja vöru
 
@@ -20,15 +32,44 @@ async function getProduct(id: number | string) : Promise<IProduct> {
   return new Promise((resolve) => resolve(product))
 }
 
-async function getProducts(limit: number, offset: number) {
-  const path = `products${
+async function getProducts(
+  limit: Number, offset: Number, category: Number, search: String
+) {
+  const page = `${
     limit
-      ? offset 
-        ? `?limit=${limit}&offset=${offset}` : `?limit=${limit}`
-      : offset
-        ? `?offset=${offset}` : ''
+    ? offset 
+      ? `?limit=${limit}&offset=${offset}`
+      : `?limit=${limit}`
+    : offset
+      ? `?offset=${offset}`
+      : ''
   }`;
-  console.log(path);
+  const isPage = `${page ? '&' : '?'}`
+  const query = `${
+    search 
+    ? category
+      ? `${isPage}search=${search}&category=${category}`
+      : `${isPage}search=${search}`
+    : category
+      ? `${isPage}category=${category}`
+      : ''
+  }`;
+
+  const path = `products/${page}${query}`;
+
+  const url = new URL(path, baseurl);
+  const result = await fetch(url.href);
+  return result.json();
+}
+
+async function getCategories(
+  limit: Number, offset: Number, category: Number
+) {
+  const id = `${category ? `/${category}` : ''}`;
+  const page = getPage(limit, offset);
+
+  const path = `categories/${id}${page}`;
+
   const url = new URL(path, baseurl);
   const result = await fetch(url.href);
   return result.json();
@@ -37,4 +78,5 @@ async function getProducts(limit: number, offset: number) {
 export {
   getProduct,
   getProducts,
+  getCategories,
 };
