8d5de217e53c3eac18505078f03289649eaba4e6
Gunnar Ingi Stefansson <gis20@hi.is>
2019-04-08 16:06:55 +0000

    Home component með category lista

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,19 +1,31 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import Helmet from 'react-helmet';
 
 import './Home.scss';
 
 import Products from '../../components/products/Products';
+import Categories from '../../components/categories/Categories';
 import Button from '../../components/button/Button';
 
 export default function Home() {
 
   return (
     <Fragment>
-      <Helmet title="Forsíða" />
-      <Products limit={6}/>
-      <Button children={'Skoða alla flokka'} />
-      {/* <Categories /> */}
+      <div className="fp">
+        <Helmet title="Forsíða" />
+        <h1 className="fp__heading">Nýjar vörur</h1>
+        <Products limit={6} offset={0}/>
+        <Link
+          to={{
+            pathname: '/categories'
+          }}
+          >
+            <Button children={'Skoða alla flokka'} />
+          </Link>
+        <h2 className="fp__heading">Skoðaðu vöruflokkana okkar</h2>
+        <Categories />
+      </div>
     </Fragment>
   );
 }
