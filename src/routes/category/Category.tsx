f23f32ce1f82a85a4c511e78c459569d68565300
Gunnar Ingi Stefansson <gis20@hi.is>
2019-04-08 16:06:33 +0000

    Category component

--- src/routes/category/Category.tsx
+++ src/routes/category/Category.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Category() {
+import Products from '../../components/products/Products';
+import Button from '../../components/button/Button';
+import Search from '../../components/search/Search';
 
+import './Category.scss';
+
+export default function CategoryRoute(props: any) {
+  const [ page, setPage ] = useState(0);
+  const [ search, setSearch ] = useState('');
+
+  const { id } = props.match.params;
   return (
-    <p>category</p>
-  )
+    <div className="category">
+      <Search />
+      <Products limit={12} offset={page} category={id} search={search}/>
+      <div className="category__page">
+        { page ? <Button onClick={() => setPage(page - 12) } children={ "Fyrri síða" } /> : null }
+        <p className="category__page__number">{`Síða ${page / 12 + 1}`}</p>
+        <Button onClick={() => setPage(page + 12) } children={ "Næsta síða" } />
+      </div>
+    </div>
+  );
 }
