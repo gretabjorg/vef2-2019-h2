9d7fbb73ae0d847aff1f27a54ccf58010fbd6023
Gunnar Ingi Stefansson <gis20@hi.is>
2019-04-09 22:26:50 +0000

    product route og component

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
-export default function Product() {
+import './Product.scss';
+export default function Product(props: any) {
+  const { title, price, image, description, category_title: category } = props;
 
   return (
-    <p>product</p>
+    <div className="product">
+      <div className="product__col">
+        <img src={image}/>
+      </div>
+      <div className="product__col">
+        <h1 className="product__title">{title}</h1>
+        <div className="product__details">
+          <p>{`Flokkur: ${category}`}</p>
+          <p>{`Verð: ${price} kr.-`}</p>
+        </div>
+          <p className="product__description">{description}</p>
+      </div>   
+    </div>
   );
+}
+
+export {
+  Product
 }
--- src/routes/product/Product.tsx
+++ src/routes/product/Product.tsx
@@ -1,7 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
-export default function Product() {
+import { Product as ProductComponent } from '../../components/product/Product';
+import Products from '../../components/products/Products';
+import useGetter from '../../api/fetchItems';
+import { getProduct, getProducts } from '../../api';
+
+export default function Product(props: any) {
+  // scrollar efst ef að það er klikkað á link
+  useEffect(() => {window.scrollTo(0, 0)});
+  const { id } = props.match.params;
+  const initialState = {
+    category_id: 0,
+    category_title: '',
+    id: 0,
+    image: '',
+    price: 0,
+    title: '',
+    description: '',
+  }
+
+  const product = useGetter(getProduct, initialState, id);
+  
+  const {
+    category_id: categoryId, category_title: categoryTitle
+  } = product;
+  
+  const { items: category } = useGetter(getProducts, initialState, 6, 0, categoryId);
+
   return (
-    <p>product</p>
+    <Fragment>
+      <ProductComponent {...product} />
+      <h2>{`Meira úr ${categoryTitle}`}</h2>
+      <Products items={category}/>
+    </Fragment>
   );
 }
