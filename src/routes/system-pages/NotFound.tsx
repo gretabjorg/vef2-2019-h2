808603f3d4f10d181c2818fa2e6c7ee833645045
Gréta Björg Unnarsdóttir <greta.bjorg30@gmail.com>
2019-04-15 17:55:21 +0000

    Not found síða

--- src/routes/system-pages/NotFound.tsx
+++ src/routes/system-pages/NotFound.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './SystemPages.scss';
 import { Link } from 'react-router-dom';
+import Button from '../../components/button/Button';
 
 export default function NotFound() {
   return (
     <div className="system-page">
-      NotFound
+      <h1 className="system-page__context">Síða fannst ekki</h1>
+      <Link className="system-page__context" to="/">Aftur á forsíðu</Link>
     </div>
   )
 }
