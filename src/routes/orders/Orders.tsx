d38d41e0da7457b245b6111d335c909a09a82fae
Gunnar Ingi Stefansson <gis20@hi.is>
2019-04-15 14:39:27 +0000

    virkni fyrir orders síðu

--- src/routes/orders/Orders.tsx
+++ src/routes/orders/Orders.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import useGetter from '../../api/fetchItems';
 
 import './Orders.scss';
+import { CurrentUser } from '../../context/currentUser';
+import { getOrders } from '../../api';
 
 export default function Orders() {
+  const { token, authenticated } = useContext(CurrentUser);
+  const { items = [] } = useGetter(getOrders, {}, token);
+
+  const orderList = items.map((order: any) => (
+    // finna góða leið til þess að linka á pöntun
+    <tr key={order.id}>
+        <td>
+          <Link to={`/orders/${order.id}`}>
+            {`Pöntun #${order.id}`}
+          </Link>
+        </td>
+        <td>{order.name}</td>
+        <td>{order.address}</td>
+        <td>{order.created}</td>
+    </tr>
+  ));
+
+
   return (
-    <p>orders</p>
+    <Fragment>
+      <h1>Þínar pantanir</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Pöntun</th>
+            <th>Nafn</th>
+            <th>Heimilisfang</th>
+            <th>Búin til</th>
+          </tr>
+        </thead>
+        <tbody>
+          { orderList }
+        </tbody>
+      </table>
+    </Fragment>
   );
 }
