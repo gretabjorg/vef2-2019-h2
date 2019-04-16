b97e79c45311579b85791d99126dcb646b767849
Gréta Björg Unnarsdóttir <greta.bjorg30@gmail.com>
2019-04-16 17:04:12 +0000

    Order Css

--- src/components/orders/Order.tsx
+++ src/components/orders/Order.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ICartLine } from '../../api/types';
+
+import './Orders.scss';
 
 function makeRows({ lines }:{ lines: ICartLine[] }) {
   let index = 1;
   
   return (
     lines.map((item: ICartLine) => (
-      <tr className="order_table__row" key={index++}>
-        <td>{item.title}</td>
+      <tr className="orders__row" key={index++}>
+        <td className="orders__th">{item.title}</td>
         <td>{`${item.price} kr.-`}</td>
         <td>{item.quantity}</td>
         <td>{item.total}</td>
@@ -21,9 +24,9 @@ function OrderInfo(
   : { id: number, name: string, address: string, created: Date }
 ) {
   return (
-  <>
-    <h1>{`Pöntun #${id}`}</h1>
     <div className="order_info">
+    <h1 className="order_info__h1">{`Pöntun #${id}`}</h1>
+    <div className="order_info__info">
       <div className="order_info__row">
         <p className="order_info__col order_info__label">Nafn</p>
         <p className="order_info__col">{ name }</p>
@@ -37,33 +40,45 @@ function OrderInfo(
         <p className="order_info__col">{ created }</p>
       </div>
     </div>
-  </>
+  </div>
   )
 }
 
 export default function OrderPage({ items }:{items: any}) {
+  const { total } = items;
   const itemTableRows = makeRows({lines: items.lines});
+  
   return (
     <>
+      <div className="order_container">
       <OrderInfo 
         id={ items.id }
         name={ items.name }
         address={ items.address }
         created={ items.created }
       />
-      <table className="order_table">
-        <thead className="order_table__header">
-          <tr className="order_table__row">
-            <th>Vara</th>
+      
+        <table className="orders">
+          <thead className="orders__head">
+            <tr>
+              <th className="orders__th">Vara</th>
             <th>Verð</th>
             <th>Fjöldi</th>
             <th>Samtals</th>
           </tr>
         </thead>
-        <tbody className="order_table__body">
+          <tbody>
           { itemTableRows }
+            <tr>
+              <td colSpan={3}/>
+              <td className="orders__strong"><strong>{ `${ total } kr.-` }</strong></td>
+            </tr>
         </tbody>
       </table>
+        <div className="order_info">
+          <Link className="orders__link" to="/orders">Aftur í pantanir</Link>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/orders/Orders.tsx
+++ src/components/orders/Orders.tsx
@@ -8,8 +8,8 @@ export default function OrdersPage({ items }:{ items: IOrder[] }) {
   const orderList = items.map((order: any) => (
     // finna góða leið til þess að linka á pöntun
     <tr className="orders__row"key={order.id}>
-        <td>
-          <Link to={`/orders/${order.id}`}>
+        <td className="orders__th">
+          <Link className="orders__link" to={`/orders/${order.id}`}>
             {`Pöntun #${order.id}`}
           </Link>
         </td>
@@ -21,21 +21,21 @@ export default function OrdersPage({ items }:{ items: IOrder[] }) {
 
 
   return (
-    <>
+    <div className="order_container">
       <h1 className="order_heading">Þínar pantanir</h1>
       <table className="orders">
         <thead className="orders__head">
           <tr>
-            <th>Pöntun</th>
+            <th className="orders__th">Pöntun</th>
             <th>Nafn</th>
             <th>Heimilisfang</th>
             <th>Búin til</th>
           </tr>
         </thead>
-        <tbody className="orders__body">
+        <tbody>
           { orderList }
         </tbody>
       </table>
-    </>
+    </div>
   );
 }
