e44e285338fb4862951dfbef3d4a1fa14ad4cd26
Gunnar Ingi Stefansson <gis20@hi.is>
2019-04-17 18:39:26 +0000

    útlit á paging

--- src/components/paging/Paging.tsx
+++ src/components/paging/Paging.tsx
@@ -6,9 +6,9 @@ export default function Paging(props: any) {
   const { hasNextPage, page: { page, setPage, limit } } = props;
   return (
     <div className="paging">
-      { page ? <Button onClick={() => setPage(page - limit) } children={ "Fyrri síða" } /> : null }
-      { page === 0 ? null : <p className="paging__number">{`Síða ${page / limit + 1}`}</p> }
-      { hasNextPage ? <Button onClick={() => setPage(page + limit) } children={ "Næsta síða" } /> : null }
+      { page ? <Button onClick={() => setPage(page - limit) } small={true} children={ "Fyrri síða" } /> : null }
+      <p className="paging__number">{`Síða ${page / limit + 1}`}</p>
+      { hasNextPage ? <Button onClick={() => setPage(page + limit) } small={true} children={ "Næsta síða" } /> : null }
     </div>
   )
 }
